2758a04ce688dcb9e39b65929cc7e5e3dbf89ebb
MatMasIt <34745996+MatMasIt@users.noreply.github.com>
2021-02-23 22:24:33 +0100

    Update StudentDialog.java

--- ArchivioAlunni/src/StudentDialog.java
+++ ArchivioAlunni/src/StudentDialog.java
@@ -2,8 +2,6 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
